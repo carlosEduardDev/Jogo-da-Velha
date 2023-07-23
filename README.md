639f3d9d36938a8a2d97a83211608f5885cb1d13
Carlos Eduardo <carlos8189.mk@gmail.com>
2023-07-23 01:00:21 -0300

    Finished application code, design, add readme and etc

---
+++ README.md
@@ -0,0 +1,37 @@
+<h1> Projeto Jogo da Velha </h1>
+<p><b>Clássico jogo da velha para jogar com 2 players!</b></p>
+
+[![Website shields.io](https://img.shields.io/website-up-down-green-red/http/shields.io.svg)](http://shields.io/)
+<a href=""> Visite o projeto! </a>
+
+<h2 id="tecnologias">Técnologias Usadas:</h2>
+
+<p> Todo o projeto foi feito do zero com HTML, SCSS e TYPESCRIPT</p>
+
+<p>
+  <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" />
+   <img src="https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white"/>
+  	<img src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white"/>
+</p>
+
+<img src="https://i.ibb.co/kB1Ckvs/Jogo-da-Velha-Google-Chrome-23-07-2023-00-51-21.png"/>
+
+
+<h2>Autor do Projeto:</h2>
+
+<h3> Carlos Eduardo :) </h3>
+
+<p> 
+  <a href="https://github.com/carlosEduardDev">
+    <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" />
+  </a> 
+  <a href="https://api.whatsapp.com/send?phone=5511974265092/">
+    <img src="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+  </a>   
+</p>
+
+<ul>
+  <li>Desenvolvedor Front-End</li>
+</ul>
+
+<p><b>Esse projeto é livre, podendo pegá-lo e utilizá-lo em seus projetos e trabalhos! <b></p>
--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class Game {
         this.winningPositions = [
             [1, 2, 3],
             [1, 4, 7],
-            [1, 5, 8],
+            [1, 5, 9],
             [2, 5, 8],
             [3, 5, 7],
             [3, 6, 9],
@@ -22,15 +22,69 @@ class Game {
         this.heldPositions = [];
         this.turnPlayer = this.turnPlayer.bind(this);
     }
+    checkWinner(btn) {
+        this.heldPositions[Number(btn.dataset.i)] = this.currentPlayer;
+        const itens = this.heldPositions
+            .map((item, i) => [item, i])
+            .filter((item) => item[0] === this.currentPlayer)
+            .map((item) => item[1]);
+        setTimeout(() => {
+            for (let pos of this.winningPositions) {
+                if (pos.every((item) => itens.includes(item))) {
+                    if (this.turn) {
+                        this.turn.innerText =
+                            "o jogador " + this.currentPlayer + " ganhou !";
+                        this.markupFields.forEach((btn) => {
+                            btn.removeEventListener("click", this.turnPlayer);
+                            setTimeout(() => {
+                                this.init();
+                                this.currentPlayer = "X";
+                                this.heldPositions = [];
+                                this.markupFields.forEach((btn) => {
+                                    btn.innerText = "";
+                                    if (this.turn)
+                                        this.turn.innerText = "Vez do Jogador: O";
+                                    btn.classList.remove("filled");
+                                });
+                            }, 2000);
+                        });
+                    }
+                }
+            }
+            if (this.heldPositions.filter((item) => item).length === 9) {
+                if (this.turn)
+                    this.turn.innerText = "EMPATE";
+                setTimeout(() => {
+                    this.init();
+                    this.currentPlayer = "X";
+                    this.heldPositions = [];
+                    this.markupFields.forEach((btn) => {
+                        btn.innerText = "";
+                        btn.classList.remove("filled");
+                    });
+                    if (this.turn)
+                        this.turn.innerText = "Vez do Jogador: O";
+                }, 2000);
+            }
+        }, 200);
+    }
     turnPlayer({ currentTarget }) {
-        this.currentPlayer === "X"
-            ? (this.currentPlayer = "O")
-            : (this.currentPlayer = "X");
+        if (this.currentPlayer === "X" &&
+            currentTarget instanceof HTMLButtonElement) {
+            this.currentPlayer = "O";
+            currentTarget.style.color = "rgb(0, 137, 161)";
+        }
+        else {
+            this.currentPlayer = "X";
+            if (currentTarget instanceof HTMLButtonElement)
+                currentTarget.style.color = "rgb(219, 40, 40)";
+        }
         if (currentTarget instanceof HTMLButtonElement && this.turn) {
             this.turn.innerText = `Vez do Jogador: ${this.currentPlayer === "X" ? "O" : "X"}`;
             currentTarget.innerText = this.currentPlayer;
             currentTarget.classList.add("filled");
             currentTarget.removeEventListener("click", this.turnPlayer);
+            this.checkWinner(currentTarget);
         }
     }
     addEvents(fields) {
